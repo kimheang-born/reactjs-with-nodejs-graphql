84811572ac2334d6cabf5e9d24700ed3ed900f66
Kimheang <kimheang135@gmail.com>
2023-10-20 13:53:17 +0700

    chore: implement get & create post

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -54,26 +54,47 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:9090/feed/posts?page=' + page, {
+    const graphqlQuery = {
+      query: `
+        {
+          posts {
+              posts {
+                  _id
+                  title
+                  content
+                  creator {
+                    name
+                  }
+                  createdAt
+              }
+              totalPosts
+          }
+        }
+      `,
+    };
+    fetch('http://localhost:9090/graphql', {
+      method: 'POST',
       headers: {
         Authorization: `Bearer ${this.props.token}`,
+        'Content-Type': 'application/json',
       },
+      body: JSON.stringify(graphqlQuery),
     })
       .then((res) => {
-        if (res.status !== 200) {
-          throw new Error('Failed to fetch posts.');
-        }
         return res.json();
       })
       .then((resData) => {
+        if (resData.errors && resData.errors[0].status === 422) {
+          throw new Error('Fetching posts failed!');
+        }
         this.setState({
-          posts: resData.posts.map((post) => {
+          posts: resData.data.posts.posts.map((post) => {
             return {
               ...post,
               imagePath: post.imageUrl,
             };
           }),
-          totalPosts: resData.totalItems,
+          totalPosts: resData.data.posts.totalPosts,
           postsLoading: false,
         });
       })
@@ -182,8 +203,8 @@ class Feed extends Component {
               (p) => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
-          } else if (prevState.posts.length < 2) {
-            updatedPosts = prevState.posts.concat(post);
+          } else {
+            updatedPosts.unshift(post);
           }
           return {
             posts: updatedPosts,
