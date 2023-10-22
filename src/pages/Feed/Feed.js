a46f47e8494056dc14297cea8bc0bcc704739dfa
Kimheang <kimheang135@gmail.com>
2023-10-22 14:44:25 +0700

    fix: pagination did not appear after creating

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -276,12 +276,14 @@ class Feed extends Component {
         };
         this.setState((prevState) => {
           let updatedPosts = [...prevState.posts];
+          let updatedTotalPosts = prevState.totalPosts;
           if (prevState.editPost) {
             const postIndex = prevState.posts.findIndex(
               (p) => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
           } else {
+            updatedTotalPosts++;
             if (prevState.posts.length >= 2) {
               updatedPosts.pop();
             }
@@ -292,6 +294,7 @@ class Feed extends Component {
             isEditing: false,
             editPost: null,
             editLoading: false,
+            totalPosts: updatedTotalPosts,
           };
         });
       })
